20a17edcb413a655b04a697c6eff033b138e8626
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2022-12-07 11:38:15 -0300

    Exercicio 03 finalizado

--- Trabalhos/Trabalho01/Exercicio_03/src/application/App.java
+++ Trabalhos/Trabalho01/Exercicio_03/src/application/App.java
@@ -1,25 +1,53 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /* 
+        Escreva uma funcão recursiva que calcule a sequencia dada por:
+                    F(1) = 1                               (1)
+                    F(2) = 2                               (2)
+                    F(n) = 2 ∗ F(n − 1) + 3 ∗ F(n − 2)     (3)
+                    */
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        
+        System.out.print("digite um numero: ");
+        num = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("F(" + num +") = " + calculaSequencia(num, num, num));
+
+        sc.close();
     }
 
-    public static int calculaSequencia(int n, int aux){
+    public static int calculaSequencia(int num, int num1, int num2){
+
         int F;
 
-        if(n == 1){
+        if(num == 1){
             F = 1;
             return F;
         }
-        if(n == 2){
-            F = 2;
+        if((num1 - 1) == 1 || (num1 - 1) == 2 && (num2 - 2) == 1 || (num2 - 2) == 2){
+            num1 = num1 - 1;
+            num2 = num2 - 2;
+            F = 2 * num1 + 3 * num2;
             return F;
+            
         }
         else{
-            aux = n - 1;
-            return 0;
+             num1 = num1 - 1;
+             num2 = num2 - 2;
+             F = calculaSequencia(num, num1, num2);
+             return F;
         }
        
     }
+    
 }
