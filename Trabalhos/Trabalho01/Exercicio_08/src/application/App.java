2528c522eb0ce1b28e9804305b854d6cecfea9c6
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2022-12-08 23:54:21 -0300

    Exercicio 08 finalizado

--- Trabalhos/Trabalho01/Exercicio_08/src/application/App.java
+++ Trabalhos/Trabalho01/Exercicio_08/src/application/App.java
@@ -1,7 +1,122 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.IntegerSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        //Apresentação
+
+        System.out.println("olá usuário!");
+        System.out.println("nesse programa você deve digitar dois conjuntos numéricos");
+        System.out.println("-duas observações-");
+        System.out.println("*os conjuntos devem estar no intervalo [0,50]");
+        System.out.println("*não devem haver numeros repetidos");
+        System.out.println("---precione ENTER para continuar---");
+        sc.nextLine();
+ 
+        //inicio
+        System.out.println();
+        System.out.print("qual o tamanho do conjunto A: ");
+        int tamA = sc.nextInt();
+
+        int[] conjA = new int[tamA];
+
+        System.out.println();
+        System.out.println("entre com os elementos do conjunto A");
+
+        for(int i = 0; i < conjA.length; i++){
+            System.out.print("-->");
+            conjA[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        System.out.print("qual o tamanho do conjunto B: ");
+        int tamB = sc.nextInt();
+
+        int[] conjB = new int[tamB];
+
+        System.out.println();
+        System.out.println("entre com os elementos do conjunto B");
+
+        for(int i = 0; i < conjB.length; i++){
+            System.out.print("-->");
+            conjB[i] = sc.nextInt();
+        }
+
+        IntegerSet setA = new IntegerSet();
+        IntegerSet setB = new IntegerSet();
+
+        //preenchendo setA 
+        for(int i = 0; i < conjA.length; i++){
+            setA.adicionarElemento(conjA[i]);
+        }
+
+        //preenchendo setB
+        for(int i = 0; i < conjB.length; i++){
+            setB.adicionarElemento(conjB[i]);
+        }
+
+        System.out.println(setA.toString());
+
+        boolean loop = true;
+        IntegerSet result = new IntegerSet();
+
+        do{
+
+            System.out.println();
+            System.out.println("escolha uma opção: ");
+            System.out.println("1 - União A e B");
+            System.out.println("2 - Interseção A e B");
+            System.out.println("3 - Diferença A-B");
+            System.out.println("4 - Diferença B-A");
+            System.out.print("-->");
+
+            int opc = sc.nextInt();
+
+            if(opc == 1){
+                result = result.uniao(setA, setB);
+
+                System.out.println();
+                System.out.println("AUB = " + result.toString());
+            }
+            if(opc == 2){
+                result = result.intersecao(setA, setB);
+
+                System.out.println();
+                System.out.println("A(intersec)B = " + result.toString());
+            }
+            if(opc == 3){
+                result = result.diferencaAB(setA, setB);
+
+                System.out.println();
+                System.out.println("A-B = " + result.toString());
+            }
+            if(opc == 4){
+                result = result.diferencaBA(setA, setB);
+
+                System.out.println();
+                System.out.println("B-A = " + result.toString());
+            }
+
+            System.out.printf("\ndeseja continuar?\n 1 - sim \n 2 - não");
+            System.out.print("\n-->");
+
+            int continuar = sc.nextInt();
+
+            if(continuar == 1){
+                loop = true;
+            }
+            else if(continuar == 2){
+                loop = false;
+            }
+
+        }while(loop == true);
+
+        sc.close();
     }
 }
--- Trabalhos/Trabalho01/Exercicio_08/src/entities/IntegerSet.java
+++ Trabalhos/Trabalho01/Exercicio_08/src/entities/IntegerSet.java
@@ -2,6 +2,78 @@ package entities;
 
 public class IntegerSet {
 
-    private static int max = 50;
-    private boolean[] set = new boolean[max];
+    private static final int max = 51; // conj [0,50]
+    private final boolean[] set = new boolean[max];
+
+    public IntegerSet() {
+        for (int i = 0; i < max; i++) {
+            set[i] = false;
+        }
+    }
+
+    public IntegerSet uniao(IntegerSet set01, IntegerSet set02) {
+        IntegerSet setOut = new IntegerSet();
+        for (int i = 0; i < max; i++) {
+            if (set01.set[i] == true || set02.set[i] == true) {
+                setOut.set[i] = true;
+            }
+        }
+        return setOut;
+    }
+
+    public IntegerSet intersecao(IntegerSet set01, IntegerSet set02) {
+        IntegerSet setOut = new IntegerSet();
+        for (int i = 0; i < max; i++) {
+            if (set01.set[i] == true && set02.set[i] == true) {
+                setOut.set[i] = true;
+            }
+        }
+        return setOut;
+    }
+
+    public IntegerSet diferencaAB(IntegerSet set01, IntegerSet set02) {
+        IntegerSet setOut = new IntegerSet();
+        for (int i = 0; i < max; i++) {
+            if (set01.set[i] == true && set02.set[i] == false) {
+                setOut.set[i] = true;
+            }
+        }
+        return setOut;
+    }
+
+    public IntegerSet diferencaBA(IntegerSet set01, IntegerSet set02) {
+        IntegerSet setOut = new IntegerSet();
+        for (int i = 0; i < max; i++) {
+            if (set01.set[i] == false && set02.set[i] == true) {
+                setOut.set[i] = true;
+            }
+        }
+        return setOut;
+    }
+
+    public void adicionarElemento(int elemento) {
+        set[elemento] = true;
+    }
+
+    public void retirarElemento(int elemento) {
+        set[elemento] = false;
+    }
+
+    public String toString() {
+
+        boolean conjVazio = true;
+
+        StringBuilder stringFinal = new StringBuilder();
+        stringFinal.append("{ ");
+
+        for (int i = 0; i < max; i++) {
+            if (set[i]) {
+                conjVazio = false;
+                stringFinal.append(i).append(' ');
+            }
+        }
+        
+        return conjVazio ? "---" : stringFinal.append('}').toString();
+    }
+
 }
