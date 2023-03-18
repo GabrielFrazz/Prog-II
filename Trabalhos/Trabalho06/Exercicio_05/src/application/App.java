4e017c1326aca5aa60fc4fbc35b8bace2484c859
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2023-03-18 02:34:54 -0300

    Exercicio_05 finalizado

--- Trabalhos/Trabalho06/Exercicio_05/src/application/App.java
+++ Trabalhos/Trabalho06/Exercicio_05/src/application/App.java
@@ -1,5 +1,6 @@
 package application;
 
+import entities.CarroPopular;
 import entities.CarroSedan;
 import entities.Cliente;
 import entities.FabricaDeCarro;
@@ -7,9 +8,22 @@ import entities.FabricaDeCarro;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        FabricaDeCarro fabricasedan = Cliente.getFactory("fiat");
+        FabricaDeCarro fabricaSedan = Cliente.getFactory("fiat");
 
-        CarroSedan sedan1 = fabricasedan.criarCarroSedan("Siena");
+        CarroSedan sedan1 = fabricaSedan.criarCarroSedan("Siena");
         sedan1.exibirInfoSedan();
-    }
+
+        CarroSedan sedan2 = fabricaSedan.criarCarroSedan("fiestasedan");
+        sedan2.exibirInfoSedan();
+
+        System.out.println("--------------------");
+
+        FabricaDeCarro fabricaPopular = Cliente.getFactory("ford");
+
+        CarroPopular popular1 = fabricaPopular.criarCarroPopular("fiesta");
+        popular1.exibirInfoPopular();
+
+        CarroPopular popular2 = fabricaPopular.criarCarroPopular("palio");
+        popular2.exibirInfoPopular();
+}
 }
